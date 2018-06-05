a183fdec6219c09af23e202471d8c3f048fd8265
Adam Sienkiewicz <adam584@onet.pl>
2018-06-05 11:57:52 +0200

    somewhat working

--- AI project gui/MainWindow.xaml.cs
+++ AI project gui/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,83 @@ namespace AI_project_gui
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog 
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+
+
+            // Set filter for file extension and default file extension 
+            //dlg.DefaultExt = ".exe";
+            //dlg.Filter = "exec Files (*.exe)|";
+
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = dlg.ShowDialog();
+
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                string filename = dlg.FileName;
+                ScriptBox.Text = filename;
+            }
+        }
+
+        private void CodeSelectClick(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog 
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = dlg.ShowDialog();
+
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                string filename = dlg.FileName;
+                CodeBox.Text = filename;
+            }
+        }
+
+        private void RunBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string file = CodeBox.Text;
+            string result = string.Empty;
+
+            try
+            {
+
+                var info = new ProcessStartInfo();
+                info.FileName = ScriptBox.Text;
+                info.WorkingDirectory = System.IO.Path.GetDirectoryName(ScriptBox.Text);
+                info.Arguments = CodeBox.Text + " " + ArgBox.Text;
+
+                info.RedirectStandardInput = false;
+                info.RedirectStandardOutput = true;
+                info.UseShellExecute = false;
+                info.CreateNoWindow = true;
+
+                using (var proc = new Process())
+                {
+                    proc.StartInfo = info;
+                    proc.Start();
+                    result = proc.StandardOutput.ReadToEnd();
+                }
+
+                ResultBlock.Text = result;
+            }
+            catch (Exception ex)
+            {
+                ResultBlock.Text = $"R Script failed:  + {result}  {ex}";
+            }
         }
     }
 }
